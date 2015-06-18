0b04d7df9d24ae0b915c2bdaf23fa8370783c9f7
chengkaihua <CHENGKAIHUA89@gmail.com>
2015-06-19 00:11:18 +0800

    create a project HelloWorld by swift

---
+++ swift/HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by 程开华 on 15/6/19.
+//  Copyright (c) 2015年 MaiziEdu. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+println("你好呀，程开华！")
+
